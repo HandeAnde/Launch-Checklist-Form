e6f7f0bc734696bdc958f7f6c5b08b6063fd7687
Courtney Anderson <iamcourtcourt@yahoo.com>
2020-09-07 16:02:41 -0500

    on updating shuttle requirements

--- script.js
+++ script.js
@@ -1,4 +1,52 @@
-// Write your JavaScript code here!
+window.addEventListener('load', function(){
+   let form = document.querySelector('form');
+   form.addEventListener('submit', function(event){
+      let pilotName = document.querySelector("input[name=pilotName]");
+      let copilotName = document.querySelector("input[name=copilotName]");
+      let fuelLevel = document.querySelector("input[name=fuelLevel]");
+      let cargoMass = document.querySelector("input[name=cargoMass]");
+      let pilotStatus = document.getElementById('pilotStatus');
+      let copilotStatus = document.getElementById('copilotStatus');
+      let fuelStatus = document.getElementById('fuelStatus');
+      let cargoStatus = document.getElementById('cargopilotStatus');
+      let faultyItems = document.getElementById('faultyItems');
+      let launchStatus = document.getElementById('launchStatus');
+
+      function noLaunch (){
+         faultyItems.style.visibility="visible";
+         launchStatus.innerHTML = `Shuttle not ready for launch`;
+         launchStatus.style.color = 'red';
+      }
+      
+      if(!pilotName.value || !copilotName.value || !fuelLevel.value || !cargoMass.value){
+         alert('All Fields Required.');
+      } else {
+         if(pilotName.value && copilotName.value){
+            pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready for launch`;
+            copilotStatus.innerHTML = `Copilot ${copilotName.value} is ready for launch`;
+         }
+   
+         if(fuelLevel.value < 10000){
+            fuelStatus.innerHTML = 'Fuel level too low for launch';
+            noLaunch();   
+         }
+
+         if(cargoMass.value < 10000){
+            fuelStatus.innerHTML = 'Fuel level too low for launch';
+            noLaunch();   
+         }
+
+   
+      }
+
+
+      
+
+
+      event.preventDefault();
+   });
+});
+
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
